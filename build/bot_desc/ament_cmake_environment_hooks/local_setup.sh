085b06d7272157d4e395febc042c770cd580500f
junglena <1623574538@qq.com>
2026-04-22 01:36:23 -0700

    ros2 launch bot_desc gazebo_sim.launch.py 加载机器人模型并add gazebo 标签

--- build/bot_desc/ament_cmake_environment_hooks/local_setup.sh
+++ build/bot_desc/ament_cmake_environment_hooks/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/jungle/ros_cpp/chapt6_ws/install/bot_desc"}
+: ${AMENT_CURRENT_PREFIX:="/home/jungle/ros_cpp/robot-/install/bot_desc"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
---
+++ install/bot_desc/share/bot_desc/launch/gazebo_sim.launch.py
@@ -0,0 +1,47 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+import os
+def generate_launch_description():
+    # 获取功能包的share路径
+    urdf_package_path = os.path.join(get_package_share_directory('bot_desc'))
+    default_xacro_path = os.path.join(urdf_package_path, 'urdf', 'mybot/labot.urdf.xacro')
+    default_gazebo_world_path=os.path.join(urdf_package_path,'world','custom_room.world')
+    #声明一个urdf目录的参数，方便修改
+    action_declare_arg_mode_path=launch.actions.DeclareLaunchArgument(
+        'model',
+        default_value=default_xacro_path,
+        description='URDF file path'
+    )
+
+    action_robot_state_publisher = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
+        parameters=[{
+            'robot_description': launch.substitutions.Command(['xacro ', launch.substitutions.LaunchConfiguration('model')])
+        }]
+    )
+
+    action_launch_gazebo = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments=[('world', default_gazebo_world_path),('verbose', 'true')]
+    )
+
+    action_spawn_entity = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'mybot'
+        ],
+        output='screen'
+    )
+    return launch.LaunchDescription([
+        action_declare_arg_mode_path,
+        action_robot_state_publisher,
+        action_launch_gazebo,
+        action_spawn_entity
+    ])
